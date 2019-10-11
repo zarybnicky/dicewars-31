f09f1fd8b5804d03f48d08829336309bd60fcdc6
Benes Karel <ibenes@fit.vutbr.cz>
2019-10-11 08:38:28 +0200

    Load the AI module programmaticaly

--- scripts/client.py
+++ scripts/client.py
@@ -4,6 +4,8 @@ import logging
 from PyQt5.QtWidgets import QApplication
 import sys
 
+import importlib
+
 from dicewars.client.game import Game
 from dicewars.client.ui import ClientUI
 
@@ -11,25 +13,9 @@ from utils import get_logging_level
 
 
 def get_ai_constructor(ai_specification):
-    if ai_specification == 1:
-        from dicewars.client.ai.ai1 import AI
-    elif ai_specification == 2:
-        from dicewars.client.ai.ai2 import AI
-    elif ai_specification == 3:
-        from dicewars.client.ai.ai3 import AI
-    elif ai_specification == 4:
-        from dicewars.client.ai.ai4 import AI
-    elif ai_specification == 5:
-        from dicewars.client.ai.ai5 import AI
-    elif ai_specification == 6:
-        from dicewars.client.ai.ai6 import AI
-    elif ai_specification == 7:
-        from dicewars.client.ai.ai7 import AI
-    else:
-        logging.error("Invalid AI specification {0}.".format(ai_specification))
-        exit(1)
+    ai_module = importlib.import_module('dicewars.client.ai.ai{}'.format(ai_specification))
 
-    return AI
+    return ai_module.AI
 
 
 def main():
