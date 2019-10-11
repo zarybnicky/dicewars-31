b7da9b0b3e5f50a3830a4fb6b97952c369da8fff
Benes Karel <ibenes@fit.vutbr.cz>
2019-10-11 08:21:16 +0200

    Separate AI selection

--- scripts/client.py
+++ scripts/client.py
@@ -8,6 +8,28 @@ from dicewars.client.game import Game
 from dicewars.client.ui import ClientUI
 
 
+def get_ai_constructor(ai_specification):
+    if ai_specification == 1:
+        from dicewars.client.ai.ai1 import AI
+    elif ai_specification == 2:
+        from dicewars.client.ai.ai2 import AI
+    elif ai_specification == 3:
+        from dicewars.client.ai.ai3 import AI
+    elif ai_specification == 4:
+        from dicewars.client.ai.ai4 import AI
+    elif ai_specification == 5:
+        from dicewars.client.ai.ai5 import AI
+    elif ai_specification == 6:
+        from dicewars.client.ai.ai6 import AI
+    elif ai_specification == 7:
+        from dicewars.client.ai.ai7 import AI
+    else:
+        logging.error("Invalid AI specification {0}.".format(ai_specification))
+        exit(1)
+
+    return AI
+
+
 def main():
     """Client side of Dice Wars
     """
@@ -18,27 +40,8 @@ def main():
     game = Game(args.address, args.port)
 
     if args.ai:
-        if args.ai == 1:
-            from dicewars.client.ai.ai1 import AI
-        elif args.ai == 2:
-            from dicewars.client.ai.ai2 import AI
-        elif args.ai == 3:
-            from dicewars.client.ai.ai3 import AI
-        elif args.ai == 4:
-            from dicewars.client.ai.ai4 import AI
-        elif args.ai == 5:
-            from dicewars.client.ai.ai5 import AI
-        elif args.ai == 6:
-            from dicewars.client.ai.ai6 import AI
-        elif args.ai == 7:
-            from dicewars.client.ai.ai7 import AI
-        else:
-            logging.error("No AI version {0}.".format(args.ai))
-            exit(1)
-
-        ai = AI(game)
+        ai = get_ai_constructor(args.ai)(game)
         ai.run()
-
     else:
         app = QApplication(sys.argv)
         ui = ClientUI(game)
