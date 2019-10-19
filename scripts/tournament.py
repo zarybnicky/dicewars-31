ea987514fca20ea9c5c38502287696d328d38fde
Karel Beneš <ibenes@fit.vutbr.cz>
2019-10-19 22:49:19 +0200

    Include all AIs in the tournament definition

--- scripts/tournament.py
+++ scripts/tournament.py
@@ -36,7 +36,12 @@ def signal_handler(signum, frame):
 
 PLAYING_AIs = [
     'dt.rand',
+    'dt.sdc',
+    'dt.ste',
     'dt.stei',
+    'dt.wpm_d',
+    'dt.wpm_s',
+    'dt.wpm_c',
     'xlogin42',
     'xlogin00',
 ]
