6b7e948739be73adf0b212770699b8d3066b554a
Karel Beneš <ibenes@fit.vutbr.cz>
2019-10-20 21:12:05 +0200

    Create an empty AI to serve as a template

---
+++ dicewars/ai/template.py
@@ -0,0 +1,12 @@
+import logging
+
+from dicewars.ai.ai_base import BattleCommand, EndTurnCommand
+
+
+class AI:
+    def __init__(self, player_name, board, players_order):
+        self.player_name = player_name
+        self.logger = logging.getLogger('AI')
+
+    def ai_turn(self, board, nb_moves_this_turn, nb_turns_this_game, previous_time_left):
+        return EndTurnCommand()
