6880ff261c5cc67f9571109ea33aba11e3a6e9be
Benes Karel <ibenes@fit.vutbr.cz>
2019-11-15 15:25:15 +0100

    Factor out EnterableTimerMixin from concrete timers

--- dicewars/client/timers.py
+++ dicewars/client/timers.py
@@ -2,7 +2,20 @@ import signal
 import logging
 
 
-class FischerTimer:
+class EnterableTimerMixin:
+    def __init__(self):
+        raise NotImplementedError
+
+    def __enter__(self):
+        logging.debug('Starting clock with {:.3f}s left'.format(self.time_left))
+        return self.run()
+
+    def __exit__(self, type, value, traceback):
+        self.stop()
+        logging.debug('Stopped clock, now with {:.3f}s left'.format(self.time_left))
+
+
+class FischerTimer(EnterableTimerMixin):
     def __init__(self, start_time, increment):
         self._time_left = start_time
         self._increment = increment
@@ -21,16 +34,8 @@ class FischerTimer:
     def time_left(self):
         return self._time_left
 
-    def __enter__(self):
-        logging.debug('Starting clock with {:.3f}s left'.format(self.time_left))
-        return self.run()
 
-    def __exit__(self, type, value, traceback):
-        self.stop()
-        logging.debug('Stopped clock, now with {:.3f}s left'.format(self.time_left))
-
-
-class FixedTimer:
+class FixedTimer(EnterableTimerMixin):
     def __init__(self, start_time):
         self._time_left = start_time
 
@@ -46,11 +51,3 @@ class FixedTimer:
     @property
     def time_left(self):
         return self._time_left
-
-    def __enter__(self):
-        logging.debug('Starting clock with {:.3f}s left'.format(self.time_left))
-        return self.run()
-
-    def __exit__(self, type, value, traceback):
-        self.stop()
-        logging.debug('Stopped clock, now with {:.3f}s left'.format(self.time_left))
